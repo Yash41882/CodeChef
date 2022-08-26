41549227a93aa3c8cb9be53b7b8945dc859562e3
Yash Sharma <76611733+Yash41882@users.noreply.github.com>
2022-08-26 12:50:11 +0530

    Create template.cpp

---
+++ template.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+#define mod 1000000007
+#define ll long long int
+#define ld long double
+
+
+void solve(){
+    
+    
+  
+    return;
+}
+
+
+
+int main() {
+	// your code goes here
+    int t;
+    cin>>t;
+    
+    while(t--)
+        solve();
+    
+	return 0;
+}
